05fa42d069bd675a5759126739bc6e5c221713c4
Cierra Ouellette <cierra3202@gmail.com>
2021-02-20 16:55:04 -0500

    Flashcards Lab 1 completed

--- Flashcards/ViewController.swift
+++ Flashcards/ViewController.swift
@@ -11,9 +11,82 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        answerLabel.clipsToBounds = true
+        answerLabel.layer.cornerRadius = 20.0
+        answerLabel.layer.cornerRadius = 20.0
+        answerLabel.layer.borderWidth = 3.0
+        answerLabel.layer.borderColor = #colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1)
+        
+        answer2Label.clipsToBounds = true
+        answer2Label.layer.cornerRadius = 20.0
+        
+        questionLabel.clipsToBounds = true
+        questionLabel.layer.cornerRadius = 20.0
+
+        optionOne.layer.cornerRadius = 20.0
+        optionOne.layer.borderWidth = 3.0
+        optionOne.layer.borderColor = #colorLiteral(red: 0.2588235438, green: 0.7568627596, blue: 0.9686274529, alpha: 1)
+        
+        optionTwo.layer.cornerRadius = 20.0
+        optionTwo.layer.borderWidth = 3.0
+        optionTwo.layer.borderColor = #colorLiteral(red: 0.2588235438, green: 0.7568627596, blue: 0.9686274529, alpha: 1)
+
+        optionThree.layer.cornerRadius = 20.0
+        optionThree.layer.borderWidth = 3.0
+        optionThree.layer.borderColor = #colorLiteral(red: 0.2588235438, green: 0.7568627596, blue: 0.9686274529, alpha: 1)
+
+        flashcard.layer.cornerRadius = 20.0
+        flashcard.layer.shadowRadius = 20.0
+        flashcard.layer.shadowOpacity = 0.4
+        
+        
     }
 
+    @IBOutlet weak var flashcard: UIView!
+    @IBOutlet weak var answerLabel: UILabel!
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var answer2Label: UILabel!
+    
+    @IBOutlet weak var optionOne: UIButton!
+    @IBOutlet weak var optionTwo: UIButton!
+    @IBOutlet weak var optionThree: UIButton!
+    
+    
+    @IBAction func didTapOnFlashcard(_ sender: Any) {
+        if(questionLabel.isHidden){
+            questionLabel.isHidden = false
+        }
+        else{
+            questionLabel.isHidden = true
+        }
+    }
 
+    
+    @IBAction func didTapOptionOne(_ sender: Any) {
+        optionOne.isHidden = true
+    }
+    
+    @IBAction func didTapOptionTwo(_ sender: Any) {
+        optionTwo.isHidden = true
+        answerLabel.isHidden = false
+    }
+    
+    @IBAction func didTapOptionThree(_ sender: Any) {
+        optionThree.isHidden = true
+    }
+    
+    /*  @IBAction func didTapOptionOne(_ sender: Any) {
+        optionOne.isHidden = true
+    }
+    
+    @IBAction func didTapOptionTwo(_ sender: Any) {
+        optionTwo.isHidden = true
+        answerLabel.isHidden = false
+    }
+    
+    @IBAction func didTapOptionThree(_ sender: Any) {
+        optionThree.isHidden = true
+    } */
 }
 
